c005351aa01921df31fbcbe1be139f31f49f2d0a
Silas Landricombe <si@siland.co.uk>
2017-09-23 22:57:36 +0100

    Added test request

--- src/handlers/__tests__/AMAZONStopIntent/__request__/index.js
+++ src/handlers/__tests__/AMAZONStopIntent/__request__/index.js
@@ -1,24 +1,40 @@
 module.exports = {
-  "session": {
-    "sessionId": "SessionId.d19dfa24-e08c-4c82-b183-c1d044262e2a",
-    "application": {
-      "applicationId": "amzn1.ask.skill.06eb881e-c4ac-4e2a-8f8e-17db127cfcda"
+    "session": {
+      "new": false,
+      "sessionId": "SessionId.44cfa07e-ca63-477d-9ed0-73f32b70ec14",
+      "application": {
+        "applicationId": "amzn1.ask.skill.802144c9-42cc-4288-910a-5acf91a37e46"
+      },
+      "attributes": {},
+      "user": {
+        "userId": "amzn1.ask.account.AHDBPLSA5XSDV33PGZYVP45CU2QFLFV7XE4RV7JP5QPRWWTUBPTWHZQ5YLIHO2F7P2BYNONQ6PFGOXZRC3SQ5B4WVI4EGT6JM6SRMT2QVXN5BYUOE6AZWZXILQBLDDTFKXMAPTYB27NXDMBG7QFFD43DYH2IJBMKMADA4C2CC4WWWN4R43MZ6DDDMEMJIFQDEVIRJZ2E2PENFRI"
+      }
     },
-    "attributes": {},
-    "user": {
-      "userId": "amzn1.ask.account.AG3DH5GSTVIIOCP43F47DHOQWTQZ6PYKZZ4M5AGTSNRIJVK7K46CHZPFHIVA6GXLGGUAMFHLE3UBNWQXNSQENQ4J6RIQRHNLTGXQGOFJ3B5XO24Z7HNOUA4DDA5PQQDZIUWUCAIF6ZD5Q3J3ODK35YRNTD5QKVDCJRPI6CCW4TXLP5XXA2D6MB2BCAC6PA2EXG55MUVRC2MWSTY"
+    "request": {
+      "type": "IntentRequest",
+      "requestId": "EdwRequestId.3a477c19-b232-456d-a936-a2a5d482855b",
+      "intent": {
+        "name": "AMAZON.StopIntent",
+        "slots": {}
+      },
+      "locale": "en-GB",
+      "timestamp": "2017-09-23T21:56:08Z"
     },
-    "new": false
-  },
-  "request": {
-    "type": "IntentRequest",
-    "requestId": "EdwRequestId.b8f7e9bf-dfc4-4b2b-b8a1-0323c4ba521c",
-    "locale": "en-GB",
-    "timestamp": "2017-03-24T13:08:33Z",
-    "intent": {
-      "name": "AMAZON.StopIntent",
-      "slots": {}
-    }
-  },
-  "version": "1.0"
-}
+    "context": {
+      "AudioPlayer": {
+        "playerActivity": "IDLE"
+      },
+      "System": {
+        "application": {
+          "applicationId": "amzn1.ask.skill.802144c9-42cc-4288-910a-5acf91a37e46"
+        },
+        "user": {
+          "userId": "amzn1.ask.account.AHDBPLSA5XSDV33PGZYVP45CU2QFLFV7XE4RV7JP5QPRWWTUBPTWHZQ5YLIHO2F7P2BYNONQ6PFGOXZRC3SQ5B4WVI4EGT6JM6SRMT2QVXN5BYUOE6AZWZXILQBLDDTFKXMAPTYB27NXDMBG7QFFD43DYH2IJBMKMADA4C2CC4WWWN4R43MZ6DDDMEMJIFQDEVIRJZ2E2PENFRI"
+        },
+        "device": {
+          "supportedInterfaces": {}
+        }
+      }
+    },
+    "version": "1.0"
+  }
